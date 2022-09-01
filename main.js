7d6e4067f5252564046980a827f7132301f3a91a
Mary <89971822+Mary-fox@users.noreply.github.com>
2022-09-01 14:24:39 +0300

    [doc]

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 let display = document.getElementById('display');
 let buttons = Array.from(document.getElementsByClassName('button')); //array.from метод,который создает массив
-
+console.log(buttons);
 
 //метод .map используется для выполнения над каждым элементом итерируемого объекта 
 //некую последовательность действий.
-buttons.map (button => {
-    button.addEventListener('click', function (e){
-        let event = e.target.innerText;
-        if (event === 'C') {
-            display.innerText = '';
-        }else if (event === '=') {
-                display.innerText = eval(display.innerText);
-        }else if (event = '←') {
-            display.innerText = display.innerText.slice(0, -1);
-        }else {event = display.innerText += e.target.innerText;
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
         }
     });
 });
